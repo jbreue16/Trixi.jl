959e7cbdc42cd033abbdc2ae9cc5213a9fa0b838
Jan Breuer <jbreue16@uni-koeln.de>
2021-04-28 15:39:42 +0200

    example copy

--- src/WR2/milestone1.jl
+++ src/WR2/milestone1.jl
@@ -1,110 +1,81 @@
 
+using OrdinaryDiffEq
+using Trixi
+using Plots
 
-function initial_condition_constant(x, t, equations::CompressibleEulerEquations2D)
-    rho = 1.0
-    rho_v1 = 0.1
-    rho_v2 = -0.2
-    rho_e = 10.0
-    return SVector(rho, rho_v1, rho_v2, rho_e)
-  end
-  
-  
-  """
-      initial_condition_convergence_test(x, t, equations::CompressibleEulerEquations2D)
-  
-  A smooth initial condition used for convergence tests in combination with
-  [`source_terms_convergence_test`](@ref)
-  (and [`boundary_condition_convergence_test`](@ref) in non-periodic domains).
-  """
-  function initial_condition_convergence_test(x, t, equations::CompressibleEulerEquations2D)
-    c = 2
-    A = 0.1
-    L = 2
-    f = 1/L
-    ω = 2 * pi * f
-    ini = c + A * sin(ω * (x[1] + x[2] - t))
-  
-    rho = ini
-    rho_v1 = ini
-    rho_v2 = ini
-    rho_e = ini^2
-  
-    return SVector(rho, rho_v1, rho_v2, rho_e)
-  end
-  
-  """
-      source_terms_convergence_test(u, x, t, equations::CompressibleEulerEquations2D)
-  
-  Source terms used for convergence tests in combination with
-  [`initial_condition_convergence_test`](@ref)
-  (and [`boundary_condition_convergence_test`](@ref) in non-periodic domains).
-  """
-  @inline function source_terms_convergence_test(u, x, t, equations::CompressibleEulerEquations2D)
-    # Same settings as in `initial_condition`
-    c = 2
-    A = 0.1
-    L = 2
-    f = 1/L
-    ω = 2 * pi * f
-    γ = equations.gamma
-  
-    x1, x2 = x
-    si, co = sincos((x1 + x2 - t)*ω)
-    tmp1 = co * A * ω
-    tmp2 = si * A
-    tmp3 = γ - 1
-    tmp4 = (2*c - 1)*tmp3
-    tmp5 = (2*tmp2*γ - 2*tmp2 + tmp4 + 1)*tmp1
-    tmp6 = tmp2 + c
-  
-    du1 = tmp1
-    du2 = tmp5
-    du3 = tmp5
-    du4 = 2*((tmp6 - 1)*tmp3 + tmp6*γ)*tmp1
-  
-    # Original terms (without performanc enhancements)
-    # du1 = cos((x1 + x2 - t)*ω)*A*ω
-    # du2 = (2*sin((x1 + x2 - t)*ω)*A*γ - 2*sin((x1 + x2 - t)*ω)*A +
-    #                             2*c*γ - 2*c - γ + 2)*cos((x1 + x2 - t)*ω)*A*ω
-    # du3 = (2*sin((x1 + x2 - t)*ω)*A*γ - 2*sin((x1 + x2 - t)*ω)*A +
-    #                             2*c*γ - 2*c - γ + 2)*cos((x1 + x2 - t)*ω)*A*ω
-    # du3 = 2*((c - 1 + sin((x1 + x2 - t)*ω)*A)*(γ - 1) +
-    #                             (sin((x1 + x2 - t)*ω)*A + c)*γ)*cos((x1 + x2 - t)*ω)*A*ω
-  
-    return SVector(du1, du2, du3, du4)
-  end
-  
-  """
-      boundary_condition_convergence_test(u_inner, orientation, direction, x, t,
-                                          surface_flux_function,
-                                          equations::CompressibleEulerEquations2D)
-  
-  Boundary conditions used for convergence tests in combination with
-  [`initial_condition_convergence_test`](@ref) and [`source_terms_convergence_test`](@ref).
-  """
-  function boundary_condition_convergence_test(u_inner, orientation, direction, x, t,
-                                                surface_flux_function,
-                                                equations::CompressibleEulerEquations2D)
-    u_boundary = initial_condition_convergence_test(x, t, equations)
-  
-    # Calculate boundary flux
-    if direction in (2, 4) # u_inner is "left" of boundary, u_boundary is "right" of boundary
-      flux = surface_flux_function(u_inner, u_boundary, orientation, equations)
-    else # u_boundary is "left" of boundary, u_inner is "right" of boundary
-      flux = surface_flux_function(u_boundary, u_inner, orientation, equations)
-    end
-  
-    return flux
-  end
+###############################################################################
+# semidiscretization of the compressible Euler equations
+
+equations = CompressibleEulerEquations2D(1.4)
+
+initial_condition = initial_condition_blast_wave
+
+surface_flux = flux_lax_friedrichs
+volume_flux  = flux_chandrashekar
+basis = LobattoLegendreBasis(3)
+indicator_sc = IndicatorHennemannGassner(equations, basis,
+                                         alpha_max=0.5,
+                                         alpha_min=0.001,
+                                         alpha_smooth=true,
+                                         variable=density_pressure)
+volume_integral = VolumeIntegralShockCapturingHG(indicator_sc;
+                                                 volume_flux_dg=volume_flux,
+                                                 volume_flux_fv=surface_flux)
+solver = DGSEM(basis, surface_flux, volume_integral)
+
+coordinates_min = (-2, -2)
+coordinates_max = ( 2,  2)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=6,
+                n_cells_max=10_000)
 
 
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver)
 
 
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 12.5)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+save_solution = SaveSolutionCallback(interval=100,
+                                     save_initial_solution=true,
+                                     save_final_solution=true,
+                                     solution_variables=cons2prim)
+
+amr_indicator = IndicatorHennemannGassner(semi,
+                                          alpha_max=0.5,
+                                          alpha_min=0.001,
+                                          alpha_smooth=true,
+                                          variable=density_pressure)
+amr_controller = ControllerThreeLevel(semi, amr_indicator,
+                                      base_level=4,
+                                      max_level =6, max_threshold=0.01)
+amr_callback = AMRCallback(semi, amr_controller,
+                           interval=5,
+                           adapt_initial_condition=true,
+                           adapt_initial_condition_only_refine=true)
+
+stepsize_callback = StepsizeCallback(cfl=0.9)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback, alive_callback, 
+                        save_solution,
+                        amr_callback, stepsize_callback)
 
 
+###############################################################################
+# run the simulation
 
-
-
-
-
-
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep=false, callback=callbacks);
+summary_callback() # print the timer summary
