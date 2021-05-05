df5a68b0824fcf23fd19b077f1d58262fed6168e
mats <mats.le@mail.de>
2021-05-05 13:26:04 +0200

    m2

--- src/solvers/dg_curved/dg_2d.jl
+++ src/solvers/dg_curved/dg_2d.jl
@@ -75,45 +75,58 @@ function calc_volume_integral!(du, u,
     return nothing
 end
 
-function calc_volume_integral!(du, u,
-                                mesh::CurvedMesh{2},
-                                nonconservative_terms::Val{false}, equations,
-                                volume_integral::VolumeIntegralStrongForm,
-                                dg::DGSEM, cache)
-    @unpack derivative_dhat = dg.basis
-    @unpack contravariant_vectors = cache.elements
+function calc_volume_integral!(du, u, 
+                              mesh::CurvedMesh{2},
+                              nonconservative_terms, equations,
+                              volume_integral::VolumeIntegralFluxDifferencing,
+                              dg::DGSEM, cache)
+    for element in eachelement(dg, cache)
+        split_form_kernel!(du, u, nonconservative_terms, equations, volume_integral.volume_flux, dg, cache, element)
+    end
+end
 
-    @threaded for element in eachelement(dg, cache)
-        for j in eachnode(dg), i in eachnode(dg)
-            u_node = get_node_vars(u, equations, dg, i, j, element)
+@inline function split_form_kernel!(du::AbstractArray{<:Any,4}, u,
+                                    nonconservative_terms::Val{false}, equations,
+                                    volume_flux, dg::DGSEM, cache,
+                                    element, alpha=true)
+# true * [some floating point value] == [exactly the same floating point value]
+# This can (hopefully) be optimized away due to constant propagation.
+    @unpack derivative_split = dg.basis
 
-            flux1 = flux(u_node, 1, equations)
-            flux2 = flux(u_node, 2, equations)
+# Calculate volume integral in one element
+    for j in eachnode(dg), i in eachnode(dg)
+        u_node = get_node_vars(u, equations, dg, i, j, element)
 
-# Compute the contravariant flux by taking the scalar product of the
-# first contravariant vector Ja^1 and the flux vector
-            Ja11, Ja12 = get_contravariant_vector(1, contravariant_vectors, i, j, element)
-            contravariant_flux1 = Ja11 * flux1 + Ja12 * flux2
+# x direction
+# use consistency of the volume flux to make this evaluation cheaper
+        flux1 = flux(u_node, 1, equations)
+        integral_contribution = alpha * derivative_split[i, i] * flux1
+        add_to_node_vars!(du, integral_contribution, equations, dg, i, j, element)
+# use symmetry of the volume flux for the remaining terms
+        for ii in (i + 1):nnodes(dg)
+            u_node_ii = get_node_vars(u, equations, dg, ii, j, element)
+            flux1 = volume_flux(u_node, u_node_ii, 1, equations)
+            integral_contribution = alpha * derivative_split[i, ii] * flux1
+            add_to_node_vars!(du, integral_contribution, equations, dg, i,  j, element)
+            integral_contribution = alpha * derivative_split[ii, i] * flux1
+            add_to_node_vars!(du, integral_contribution, equations, dg, ii, j, element)
+        end
 
-            for ii in eachnode(dg)
-                integral_contribution = derivative_dhat[ii, i] * contravariant_flux1
-                add_to_node_vars!(du, integral_contribution, equations, dg, ii, j, element)
-            end
-
-# Compute the contravariant flux by taking the scalar product of the
-# second contravariant vector Ja^2 and the flux vector
-            Ja21, Ja22 = get_contravariant_vector(2, contravariant_vectors, i, j, element)
-            contravariant_flux2 = Ja21 * flux1 + Ja22 * flux2
-
-            for jj in eachnode(dg)
-                integral_contribution = derivative_dhat[jj, j] * contravariant_flux2
-                add_to_node_vars!(du, integral_contribution, equations, dg, i, jj, element)
-            end
+# y direction
+# use consistency of the volume flux to make this evaluation cheaper
+        flux2 = flux(u_node, 2, equations)
+        integral_contribution = alpha * derivative_split[j, j] * flux2
+        add_to_node_vars!(du, integral_contribution, equations, dg, i, j, element)
+# use symmetry of the volume flux for the remaining terms
+        for jj in (j + 1):nnodes(dg)
+            u_node_jj = get_node_vars(u, equations, dg, i, jj, element)
+            flux2 = volume_flux(u_node, u_node_jj, 2, equations)
+            integral_contribution = alpha * derivative_split[j, jj] * flux2
+            add_to_node_vars!(du, integral_contribution, equations, dg, i, j,  element)
+            integral_contribution = alpha * derivative_split[jj, j] * flux2
+            add_to_node_vars!(du, integral_contribution, equations, dg, i, jj, element)
         end
     end
-    # println("strong")
-    return nothing
-    
 end
 
 
