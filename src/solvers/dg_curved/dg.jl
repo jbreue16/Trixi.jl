bdb0d3e2cf47102871eeb9887c9526ef2f931871
Jan Breuer <j.m.br@gmx.de>
2021-07-13 20:49:39 +0200

    quickfix for periodic boundary

--- src/solvers/dg_curved/dg.jl
+++ src/solvers/dg_curved/dg.jl
@@ -2,24 +2,24 @@
 # It constructs the basic `cache` used throughout the simulation to compute the RHS etc.
 # Additionally the cache can contain the gradient matrices for Euler equation with viscous terms
 
-function create_cache(mesh::CurvedMesh, equations::AbstractEquations, dg::DG, ::Any, ::Type{uEltype}) where {uEltype<:Real}
-  elements = init_elements(mesh, equations, dg.basis, uEltype)
+function create_cache(mesh::CurvedMesh, equations::AbstractEquations, dg::DG, ::Any, ::Type{uEltype}) where {uEltype <: Real}
+    elements = init_elements(mesh, equations, dg.basis, uEltype)
 
-  if typeof(equations) == CompressibleEulerEquations2D{Float64} && equations.viscous == true
-      q1 = zeros(nvariables(equations), length(dg.basis.nodes), length(dg.basis.nodes), size(mesh, 1)*size(mesh, 2))
-      q2 = zeros(nvariables(equations), length(dg.basis.nodes), length(dg.basis.nodes), size(mesh, 1)*size(mesh, 2))
-      cache = (; elements, q1, q2)
-  else
-    cache = (; elements)
-  end
+    if typeof(equations) == CompressibleEulerEquations2D{Float64} && equations.viscous == true
+        q1 = zeros(nvariables(equations), length(dg.basis.nodes), length(dg.basis.nodes), size(mesh, 1) * size(mesh, 2))
+        q2 = zeros(nvariables(equations), length(dg.basis.nodes), length(dg.basis.nodes), size(mesh, 1) * size(mesh, 2))
+        cache = (; elements, q1, q2)
+    else
+        cache = (; elements)
+    end
 
-  return cache
+    return cache
 end
 
 # Extract contravariant vector Ja^i (i = index) as SVector
 @inline function get_contravariant_vector(index, contravariant_vectors, indices...)
 
-  SVector(ntuple(dim -> contravariant_vectors[dim, index, indices...], ndims(contravariant_vectors) - 3))
+    SVector(ntuple(dim -> contravariant_vectors[dim, index, indices...], ndims(contravariant_vectors) - 3))
 end
 
 
@@ -27,7 +27,13 @@ end
                                                   boundary_condition::BoundaryConditionPeriodic,
                                                   mesh::CurvedMesh, equations, dg::DG, cache,
                                                   direction, node_indices, surface_node_indices, element)
-  @assert isperiodic(mesh, orientation)
+    @assert isperiodic(mesh, orientation)
+end
+@inline function calc_boundary_flux_by_direction!(surface_flux_values, u, t, orientation,
+                                                  boundary_condition::BoundaryConditionPeriodic,
+                                                  mesh::CurvedMesh, equations::AuxiliaryEquation, dg::DG, cache,
+                                                  direction, node_indices, surface_node_indices, element, nabla)
+    @assert isperiodic(mesh, orientation)
 end
 
 # BLZ: ersetzt und in dg_2d gepackt
