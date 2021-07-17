42f14a9a9207fd7b0fc6a5fc9cd098b45228b604
mats <mats.le@mail.de>
2021-07-17 23:55:03 +0200

    Gauss ist richtungsweisend

--- src/WR2/MS5 Gauss.jl
+++ src/WR2/MS5 Gauss.jl
@@ -5,10 +5,10 @@ using Plots
 
 # Vgl. Gauss on an cartesian Grid with coordinates (-1,-1),(1,1) with periodic BC
 ###############################################################################
-CFL = 0.005       # 2
-tspan = (0.0, 5)
-N = 3
-c = 16
+CFL = 0.01
+tspan = (0.0, 6)
+N = 9
+c = 2
 mu = 0.001
 
 
@@ -23,10 +23,21 @@ function initial_condition_gauss(x, t, equations::CompressibleEulerEquations2D)
     return SVector(rho, rho_v1, rho_v2, rho_e)
 end
 
+function initial_condition_density_pulse_new(x, t, equations::CompressibleEulerEquations2D)
+    rho = 1 + exp(-(x[1]^2 + x[2]^2)) / 2
+    v1 = 0
+    v2 = 1
+    rho_v1 = rho * v1
+    rho_v2 = rho * v2
+    p = 1
+    rho_e = p / (equations.gamma - 1) + 1 / 2 * rho * (v1^2 + v2^2)
+    return SVector(rho, rho_v1, rho_v2, rho_e)
+    end
+
 
 equations = CompressibleEulerEquations2D(1.4, viscous = true, mu = mu)
 
-initial_condition = initial_condition_density_pulse
+initial_condition = initial_condition_density_pulse_new
 
 # boundary_conditions = (x_neg=boundary_condition_periodic,
 #                        x_pos=boundary_condition_periodic,
@@ -37,7 +48,7 @@ initial_condition = initial_condition_density_pulse
 cells_per_dimension = (c, c)
 mesh = CurvedMesh(cells_per_dimension, (-1.0, -1.0), (1.0, 1.0))
 
-surface_flux = FluxPlusDissipation(flux_chandrashekar, DissipationLocalLaxFriedrichs(max_abs_speed_naive))
+surface_flux = flux_lax_friedrichs #FluxPlusDissipation(flux_chandrashekar, DissipationLocalLaxFriedrichs(max_abs_speed_naive))
 basis = LobattoLegendreBasis(N)
 volume_flux  = flux_chandrashekar 
 volume_integral = VolumeIntegralFluxDifferencing(volume_flux)
@@ -53,7 +64,7 @@ ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 
-analysis_interval = 100
+analysis_interval = 500
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_analysis=true,
                                       # extra_analysis_errors=(:conservation_error,),
                                       # extra_analysis_integrals=(entropy, energy_total,
@@ -62,7 +73,7 @@ analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_anal
 
 alive_callback = AliveCallback(analysis_interval=analysis_interval)
 #visualization = VisualizationCallback(interval=28, plot_creator=Trixi.save_plot)
-visualization = VisualizationCallback(interval=60,variable_names=["rho"], plot_creator=Trixi.save_plot)    
+visualization = VisualizationCallback(interval=500,variable_names=["rho"], plot_creator=Trixi.save_plot, clims=(1.0, 1.5))    
 
 save_solution = SaveSolutionCallback(interval=50,
                                      save_initial_solution=true,
